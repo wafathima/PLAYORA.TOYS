86d56ed9b80060b3b2d110f8e7daf9d5f882c030
wafa fathima <wafathima15@gmail.com>
2026-05-15 21:07:30 +0530

    Update vite.config.js

--- appX/vite.config.js
+++ appX/vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
     tailwindcss(),
   ],
   server:{
-    port:3000
+    port:3000,
+    proxy : {
+      '/api':{
+      target : 'https://playora-backend.onrender.com/',
+      changeOrigin : true,
+  }
+  }
   }
 })
